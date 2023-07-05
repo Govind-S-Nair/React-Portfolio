77e32faa5876e22dce85e60d2069b4c846935590
Govind S Nair <115881229+Govind-S-Nair@users.noreply.github.com>
2023-07-05 22:15:20 +0530

    Updated Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import HTML from '../assets/HTML.png';
-import CSS from '../assets/CSS.png';
+import Python from '../assets/Python.png';
+import Java from '../assets/Java.png';
 import JavaScript from '../assets/JavaScript.png';
-import ReactJs from '../assets/React.png';
-import NodeJs from '../assets/Node.png';
-import Tailwind_CSS from '../assets/Tailwind_CSS.png';
+import MySQL from '../assets/MySQL.png';
+import PostgreSQL from '../assets/PostgreSQL.png';
+import JupyterLab from '../assets/jupyter.png';
 import Git from '../assets/Git.png';
 import GitHub from '../assets/GitHub.png';
 
@@ -19,28 +19,28 @@ const Skills = () => {
                 </div>
                 <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
                     <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-                        <img className="w-20 mx-auto" src={HTML} alt="HTML" />
-                        <p className="my-4">HTML</p>
+                        <img className="w-20 mx-auto" src={Python} alt="Python" />
+                        <p className="my-4">Python</p>
                     </div>
                     <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-                        <img className="w-20 mx-auto" src={CSS} alt="CSS" />
-                        <p className="my-4">CSS</p>
+                        <img className="w-20 mx-auto" src={Java} alt="Java" />
+                        <p className="my-4">Java</p>
                     </div>
                     <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
                         <img className="w-20 mx-auto" src={JavaScript} alt="JavaScript" />
                         <p className="my-4">JavaScript</p>
                     </div>
                     <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-                        <img className="w-20 mx-auto" src={ReactJs} alt="ReactJs" />
-                        <p className="my-4">React.Js</p>
+                        <img className="w-20 mx-auto" src={MySQL} alt="MySQL" />
+                        <p className="my-4">MySQL</p>
                     </div>
                     <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-                        <img className="w-20 mx-auto" src={NodeJs} alt="NodeJs" />
-                        <p className="my-4">Node.Js</p>
+                        <img className="w-20 mx-auto" src={PostgreSQL} alt="PostgreSQL" />
+                        <p className="my-4">PostgreSQL</p>
                     </div>
                     <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-                        <img className="w-20 mx-auto" src={Tailwind_CSS} alt="Tailwind CSS" />
-                        <p className="my-4">Tailwind CSS</p>
+                        <img className="w-20 mx-auto" src={JupyterLab} alt="JupyterLab" />
+                        <p className="my-4">JupyterLab</p>
                     </div>
                     <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
                         <img className="w-20 mx-auto" src={Git} alt="Git" />
