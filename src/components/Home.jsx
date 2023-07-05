42a118f716a0cb55f232a22e57646de55214b641
Govind S Nair <115881229+Govind-S-Nair@users.noreply.github.com>
2023-07-05 17:05:13 +0530

    Updated Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
             <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
                 <p className="text-[#009867] text-3xl">Hi, my name is</p>
                 <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Govind S Nair</h1>
-                <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a Front-End Developer.</h2>
+                <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a data enthusiast.</h2>
                 <p className="text-[#ccd6f6] text-xl py-4 max-w-[700px]">
                 I'm an aspiring data analyst with a keen eye for detail and a passion for problem-solving. 
                 I strive to provide meaningful insights by continuously learning and staying up to date with industry trends. 
