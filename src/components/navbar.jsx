552d06595b1895d23debeaef00267ee220da2d54
Govind S Nair <115881229+g0v1ndN@users.noreply.github.com>
2023-07-26 18:00:46 +0530

    Updated navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = () => {
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0] '>
                         <a className='flex justify-between items-center w-full text-gray-300'
-                        href="mailto:gnair4363@gmail.com">
+                        href="mailto:govindsnair23@gmail.com">
                             Mail <HiOutlineMail size={30}/>
                         </a>
                     </li>
