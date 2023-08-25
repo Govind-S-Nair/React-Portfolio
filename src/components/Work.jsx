86f847c62bcc177ab259520f052732e3471f64ab
Govind S Nair <115881229+g0v1ndN@users.noreply.github.com>
2023-08-25 22:47:45 +0530

    Updated Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import WorkImg from "../assets/work.jpeg";
-
+import VirusInsights from "../assets/VirusInsights.png;
 
 const Work = () => {
     return(
@@ -14,19 +14,19 @@ const Work = () => {
                 {/*Container*/}
                 <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
                     {/*Grid Item*/}
-                    <div style={{backgroundImage: `url(${WorkImg})`}}
+                    <div style={{backgroundImage: `url(${VirusInsights})`}}
                     className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
                         
                         {/*Hover Effects */}
                         <div className="opacity-0 group-hover:opacity-100">
                             <span className="text-2xl font-bold text-white tracking-wider">
-                                Project #1
+                                Covid SQL Analytics
                             </span>
                             <div className="pt-8 text-center">
-                                <a href="/">
+                                <a href="https://github.com/g0v1ndN/Covid-SQL-Analytics">
                                     <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
                                 </a>
-                                <a href="/">
+                                <a href="https://github.com/g0v1ndN/Covid-SQL-Analytics/blob/main/covid_data_analysis.sql">
                                     <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
                                 </a>
                             </div>
