f8cc4b3e02645fe7e212e3ab4cc351715657acd5
Govind S Nair <115881229+g0v1ndN@users.noreply.github.com>
2024-01-04 22:14:59 +0530

    Updated Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import WorkImg from "../assets/work.jpeg";
 import VirusInsights from "../assets/analyze_covid.png";
-import StockMarket from "../assets/StockMarketNew.png";
 
 const Work = () => {
     return(
